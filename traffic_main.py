67736284e73649e1ced3615920dc096b9af8a9b0
green-mint <saad.521509@gmail.com>
2020-12-05 09:41:38 +0500

    pushed local to remote

--- traffic_main.py
+++ traffic_main.py
@@ -1,1 +1,216 @@
 import cv2
+import numpy as np
+import argparse
+import imutils
+import time
+import os
+from tracker import Sort
+from utils import *
+
+
+line = [(300, 400), (900, 400)]
+counter = 0
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output video")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join(["./yolov3", "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join(["./yolov3", "yolov3.weights"])
+configPath = os.path.sep.join(["./yolov3", "yolov3.cfg"])
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(0)
+COLORS = np.random.randint(0, 255, size=(200, 3), dtype="uint8")
+
+
+# and determine only the *output* layer names that we need from YOLO
+print("loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(args["input"])
+writer = None
+(W, H) = (None, None)
+
+try:
+	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+		else cv2.CAP_PROP_FRAME_COUNT
+	total = int(vs.get(prop))
+	print("[INFO] {} total frames in video".format(total))
+
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+
+frameIndex = 0
+memory = {}
+tracker = Sort()
+
+# loop over frames from the video
+while True:
+	# read the next frame from the video
+	(read, frame) = vs.read()
+
+	
+	if not read: #in case the frame was not read means that the video has ended
+		break 
+
+	# should be true only for the first frame
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	# blob is the preprocessed(scaled, resized) image frame
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob) #giving the model the frame as input
+	start = time.time()
+	layerOutputs = net.forward(ln) #model return the outputs 
+	end = time.time()
+
+	# initialize our lists of detected bounding boxes, confidences,
+	# and class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > args["confidence"]:
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates,
+				# confidences, and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+	
+	dets = []
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+			dets.append([x, y, x+w, y+h, confidences[i]])
+
+	np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+	dets = np.asarray(dets)
+	tracks = tracker.update(dets)
+
+	boxes = []
+	indexIDs = []
+	c = []
+	previous = memory.copy()
+	memory = {}
+
+	for track in tracks:
+		boxes.append([track[0], track[1], track[2], track[3]])
+		indexIDs.append(int(track[4]))
+		memory[indexIDs[-1]] = boxes[-1]
+
+	if len(boxes) > 0:
+		i = int(0)
+		for box in boxes:
+			# extract the bounding box coordinates
+			(x, y) = (int(box[0]), int(box[1]))
+			(w, h) = (int(box[2]), int(box[3]))
+
+			# draw a bounding box rectangle and label on the image
+			# color = [int(c) for c in COLORS[classIDs[i]]]
+			# cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+			color = [int(c) for c in COLORS[indexIDs[i] % len(COLORS)]]
+			cv2.rectangle(frame, (x, y), (w, h), color, 2)
+
+			if indexIDs[i] in previous:
+				previous_box = previous[indexIDs[i]]
+				(x2, y2) = (int(previous_box[0]), int(previous_box[1]))
+				(w2, h2) = (int(previous_box[2]), int(previous_box[3]))
+				p0 = (int(x + (w-x)/2), int(y + (h-y)/2))
+				p1 = (int(x2 + (w2-x2)/2), int(y2 + (h2-y2)/2))
+				cv2.line(frame, p0, p1, color, 3)
+
+				if intersect(p0, p1, line[0], line[1]):
+					counter += 1
+
+			# text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+			text = "{}".format(indexIDs[i])
+			cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+			i += 1
+
+	# line
+	cv2.line(frame, line[0], line[1], (0, 255, 255), 5)
+
+	# counter
+	cv2.putText(frame, str(counter), (100,200), cv2.FONT_HERSHEY_DUPLEX, 5.0, (0, 255, 255), 10)
+
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(frame.shape[1], frame.shape[0]), True)
+
+
+	# write the output frame to disk
+	writer.write(frame)
+
+	# increase frame index
+	frameIndex += 1
+
+	if frameIndex >= 4000:
+		print("[INFO] cleaning up...")
+		writer.release()
+		vs.release()
+		exit()
+
+# release the file pointers
+print("cleaning up...")
+writer.release()
+vs.release()
